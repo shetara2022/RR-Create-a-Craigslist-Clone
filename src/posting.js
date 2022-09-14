66a2601169bee334a49f081ff0871716f5c71862
shetara2022 <smithshetarall@gmail.com>
2022-09-13 21:17:26 -0400

    added alt tag to image

--- src/posting.js
+++ src/posting.js
@@ -1,7 +1,7 @@
 function Posting(props) {
     return (
         <div>
-            <img src={props.posting.imageURL} />
+            <img src={props.posting.imageURL} alt={props.posting.title} />
             <h3>{props.posting.title} {props.posting.price} </h3>
             <p>{props.posting.description}</p>
 
